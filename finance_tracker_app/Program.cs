051e04f20f42a5003a1e4fe0e77443792815e4ca
MohamedAmin1996 <techlogic096@gmail.com>
2025-08-26 14:21:44 +0200

    Created the solution, app project and xUnit test project

---
+++ finance_tracker_app/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace finance_tracker_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ finance_tracker_test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace finance_tracker_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
